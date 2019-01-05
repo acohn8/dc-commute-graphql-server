a454850375a94e25c498e97c360269d9d7eccea1
Adam Cohn <adamcohn88@gmail.com>
2019-01-05 17:20:25 -0500

    basic server

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,12 +1,19 @@
 import Sequelize from 'sequelize';
+require('dotenv').config();
 
-const sequelize = new Sequelize('commuter-api_development', 'adamcohn', null, {
-  host: 'localhost',
-  dialect: 'postgres',
-  define: {
-    timestamps: false,
-    underscored: true
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  null,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    define: {
+      timestamps: false,
+      underscored: true
+    },
+    operatorsAliases: false
   }
-});
+);
 
 export default sequelize;
